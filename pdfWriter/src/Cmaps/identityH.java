358bca78020beb75043c8fb9d5f54e03467b37fd
Charles Cope <charlescope@msn.com>
2016-11-01 10:16:13 -0400

    Adding Fonts to Resources

--- pdfWriter/src/Cmaps/identityH.java
+++ pdfWriter/src/Cmaps/identityH.java
@@ -4,6 +4,11 @@ package Cmaps;
  * This Cmap is used as a \ToUnicode Cmap for true type fonts.
  */
 public class identityH {
-
+/**
+ * The constructors  for the class
+ */
+	public identityH(){
+		
+	}
 	
 }
